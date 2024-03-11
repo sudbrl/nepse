3203f1d34f9a67d1ac29830f4bba0a756100a7b2
sudbrl <36448052+sudbrl@users.noreply.github.com>
2024-03-11 13:49:38 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,26 +25,28 @@ def get_floorsheet_data(as_of=None):
         response = requests.get(url)
 
         # Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the JSON data from the response
-    data = response.json()
+        if response.status_code == 200:
+            # Parse the JSON data from the response
+            data = response.json()
 
-    # Check if 'data' is empty, indicating no more records
-    if not data['data']:
-        break
+            # Check if 'data' is empty, indicating no more records
+            if not data['data']:
+                break
 
-    # Extract information from the data
-    current_data = pd.DataFrame(data['data'])
+            # Extract information from the data
+            current_data = pd.DataFrame(data['data'])
 
-    # Update as_of with the latest value
-    as_of = data.get("as_of")
+            # Update as_of with the latest value
+            as_of = data.get("as_of")
 
-    # Append the current data to the list
-    all_data_list.append(current_data)
+            # Append the current data to the list
+            all_data_list.append(current_data)
 
-    # Increment the page number for the next iteration
-    page_number += 1
-
+            # Increment the page number for the next iteration
+            page_number += 1
+        else:
+            st.error(f"Successfull")
+            break
 
     # Concatenate all DataFrames in the list
     all_data = pd.concat(all_data_list, ignore_index=True)
